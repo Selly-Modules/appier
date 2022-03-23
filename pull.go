7356fea139a457822fac5ccfaec62cf8d9db2d5c
Minh Nguyen <minh.milano1511@gmail.com>
2022-03-23 17:03:44 +0700

    update

--- pull.go
+++ pull.go
@@ -12,3 +12,38 @@ func (Pull) ProductUpsert(payload []byte) (bool, error) {
 func (Pull) BrandUpsert(payload []byte) (bool, error) {
 	return publishWithJetStream(JetStreamAppierService, SubjectPullBrandUpsert, payload)
 }
+
+// CategoryUpsert ...
+func (Pull) CategoryUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullCategoryUpsert, payload)
+}
+
+// SubCategoryUpsert ...
+func (Pull) SubCategoryUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullSubCategoryUpsert, payload)
+}
+
+// SupplierUpsert ...
+func (Pull) SupplierUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullSupplierUpsert, payload)
+}
+
+// InventoryUpsert ...
+func (Pull) InventoryUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullInventoryUpsert, payload)
+}
+
+// SKUUpsert ...
+func (Pull) SKUUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullSKUUpsert, payload)
+}
+
+// PropertyUpsert ...
+func (Pull) PropertyUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullPropertyUpsert, payload)
+}
+
+// PropertyValueUpsert ...
+func (Pull) PropertyValueUpsert(payload []byte) (bool, error) {
+	return publishWithJetStream(JetStreamAppierService, SubjectPullPropertyValueUpsert, payload)
+}
