bad9acc2e08472144c15874620ef83c0eb908790
Minh Nguyen <minh.milano1511@gmail.com>
2022-03-25 15:15:51 +0700

    fix

--- sync_to_service.go
+++ sync_to_service.go
@@ -37,40 +37,57 @@ func handleSyncDataToServiceAppier(pattern string, typeData string) {
 	fmt.Println("keyProductPattern", pattern)
 
 	for {
-		keys, values := redisdb.GetWithPrefixPattern(pattern, limitGetKeyRedis)
+		keys, values := redisdb.GetWithPrefixPattern(pattern)
 		if len(keys) == 0 {
 			return
 		}
 
-		fmt.Println("keys:", keys)
-		fmt.Println("values", values)
-
-		payload := toBytes(values)
-		// Convert data
-		switch typeData {
-		case RedisSyncProduct:
-			Pull{}.ProductUpsert(payload)
-		case RedisSyncSKU:
-			Pull{}.SKUUpsert(payload)
-		case RedisSyncCategory:
-			Pull{}.CategoryUpsert(payload)
-		case RedisSyncSubCategory:
-			Pull{}.SubCategoryUpsert(payload)
-		case RedisSyncInventory:
-			Pull{}.InventoryUpsert(payload)
-		case RedisSyncBrand:
-			Pull{}.BrandUpsert(payload)
-		case RedisSyncSupplier:
-			Pull{}.SupplierUpsert(payload)
-		case RedisSyncProperty:
-			Pull{}.PropertyUpsert(payload)
-		case RedisSyncPropertyValue:
-			Pull{}.PropertyValueUpsert(payload)
-		}
+		processSyncToServiceAppier(typeData, values)
 
 		// Del keys
 		for _, key := range keys {
 			redisdb.DelKey(context.Background(), key)
 		}
+	}
+}
+
+// processSyncToServiceAppier ...
+func processSyncToServiceAppier(typeData string, values []string) {
+	for {
+		if len(values) <= 20 {
+			syncDataByList(typeData, values)
+			break
+		}
+
+		var list = values[0:19]
+		syncDataByList(typeData, list)
+		values = values[20:]
+	}
+	return
+}
+
+// syncDataByList ...
+func syncDataByList(typeData string, data []string) {
+	payload := toBytes(data)
+	// Convert data
+	switch typeData {
+	case RedisSyncProduct:
+		Pull{}.ProductUpsert(payload)
+	case RedisSyncSKU:
+		Pull{}.SKUUpsert(payload)
+	case RedisSyncCategory:
+		Pull{}.CategoryUpsert(payload)
+	case RedisSyncSubCategory:
+		Pull{}.SubCategoryUpsert(payload)
+	case RedisSyncInventory:
+		Pull{}.InventoryUpsert(payload)
+	case RedisSyncBrand:
+		Pull{}.BrandUpsert(payload)
+	case RedisSyncSupplier:
+		Pull{}.SupplierUpsert(payload)
+	case RedisSyncProperty:
+		Pull{}.PropertyUpsert(payload)
+	case RedisSyncPropertyValue:
+		Pull{}.PropertyValueUpsert(payload)
 	}
 }
